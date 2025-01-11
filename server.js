fa8821943976f93b0a361e876d960a67cfe29523
Madhusudan <madhusudanchauhan62@gmail.com>
2025-01-11 23:09:48 +0530

    commit

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
 const PORT = 5000;
+
+const API_KEY = process.env.NEWS_API_KEY;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('News Aggregator App Backend is running!');
+});
+
+app.get('/news', async (req, res) => {
+    try {
+        const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+            params: {
+                country: 'us',
+                apiKey: API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching news');
+    }
 });
 
 app.listen(PORT, () => {
